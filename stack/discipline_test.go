4d6d7d33242aeec35c3b2a37ac700439a0a29120
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2023-07-11 21:29:50 +0300

    improve coverage

--- stack/discipline_test.go
+++ stack/discipline_test.go
@@ -1,8 +1,10 @@
 package stack
 
 import (
+	"context"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -51,4 +53,191 @@ func TestDiscipline(t *testing.T) {
 	wg.Wait()
 
 	require.Equal(t, inSequence, outSequence)
+}
+
+func TestDisciplineOptsValidation(t *testing.T) {
+	opts := Opts[uint]{
+		StackSize: 10,
+	}
+
+	_, err := New(opts)
+	require.Error(t, err)
+
+	opts = Opts[uint]{
+		Input: make(chan uint),
+	}
+
+	_, err = New(opts)
+	require.Error(t, err)
+
+	opts = Opts[uint]{
+		Input:     make(chan uint),
+		StackSize: 10,
+		Timeout:   2 * time.Nanosecond,
+	}
+
+	_, err = New(opts)
+	require.Error(t, err)
+}
+
+func TestDisciplineTimeout(t *testing.T) {
+	quantity := 105
+	pauseAt := quantity / 2
+
+	input := make(chan uint)
+
+	opts := Opts[uint]{
+		Input:     input,
+		StackSize: 10,
+		Timeout:   500 * time.Millisecond,
+	}
+
+	discipline, err := New(opts)
+	require.NoError(t, err)
+
+	wg := &sync.WaitGroup{}
+
+	wg.Add(2)
+
+	inSequence := make([]uint, 0, quantity)
+
+	go func() {
+		defer wg.Done()
+		defer close(input)
+
+		for stage := 1; stage <= quantity; stage++ {
+			if stage == pauseAt {
+				time.Sleep(4 * opts.Timeout)
+			}
+
+			inSequence = append(inSequence, uint(stage))
+
+			input <- uint(stage)
+		}
+	}()
+
+	outSequence := make([]uint, 0, quantity)
+
+	go func() {
+		defer wg.Done()
+
+		for stack := range discipline.Output() {
+			require.NotEqual(t, 0, stack)
+			outSequence = append(outSequence, stack...)
+		}
+	}()
+
+	wg.Wait()
+
+	require.Equal(t, inSequence, outSequence)
+}
+
+func TestDisciplineStop(t *testing.T) {
+	quantity := 105
+	stopAt := quantity / 2
+
+	input := make(chan uint)
+
+	opts := Opts[uint]{
+		Input:     input,
+		StackSize: 10,
+	}
+
+	discipline, err := New(opts)
+	require.NoError(t, err)
+
+	wg := &sync.WaitGroup{}
+
+	wg.Add(2)
+
+	inSequence := make([]uint, 0, quantity)
+
+	go func() {
+		defer wg.Done()
+		defer close(input)
+
+		for stage := 1; stage <= quantity; stage++ {
+			if stage == stopAt {
+				discipline.Stop()
+				return
+			}
+
+			inSequence = append(inSequence, uint(stage))
+
+			input <- uint(stage)
+		}
+	}()
+
+	outSequence := make([]uint, 0, quantity)
+
+	go func() {
+		defer wg.Done()
+
+		for stack := range discipline.Output() {
+			require.NotEqual(t, 0, stack)
+			outSequence = append(outSequence, stack...)
+		}
+	}()
+
+	wg.Wait()
+
+	require.NotEqual(t, inSequence, outSequence)
+	require.GreaterOrEqual(t, len(outSequence), stopAt*90/100)
+}
+
+func TestDisciplineCtx(t *testing.T) {
+	quantity := 105
+	stopAt := quantity / 2
+
+	input := make(chan uint)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	opts := Opts[uint]{
+		Ctx:       ctx,
+		Input:     input,
+		StackSize: 10,
+	}
+
+	discipline, err := New(opts)
+	require.NoError(t, err)
+
+	wg := &sync.WaitGroup{}
+
+	wg.Add(2)
+
+	inSequence := make([]uint, 0, quantity)
+
+	go func() {
+		defer wg.Done()
+		defer close(input)
+
+		for stage := 1; stage <= quantity; stage++ {
+			if stage == stopAt {
+				cancel()
+				return
+			}
+
+			inSequence = append(inSequence, uint(stage))
+
+			input <- uint(stage)
+		}
+	}()
+
+	outSequence := make([]uint, 0, quantity)
+
+	go func() {
+		defer wg.Done()
+
+		for stack := range discipline.Output() {
+			require.NotEqual(t, 0, stack)
+			outSequence = append(outSequence, stack...)
+		}
+	}()
+
+	wg.Wait()
+
+	require.NotEqual(t, inSequence, outSequence)
+	require.GreaterOrEqual(t, len(outSequence), stopAt*90/100)
 }
