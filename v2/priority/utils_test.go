d4c7f9d2a9c3d41111611f334a6a4af7e3319df7
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2023-12-02 21:46:20 +0300

    add tests for CalcDistributionQuantity

--- v2/priority/utils_test.go
+++ v2/priority/utils_test.go
@@ -1,9 +1,24 @@
 package priority
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCalcDistributionQuantity(t *testing.T) {
-
+	require.Equal(t, uint(0), calcDistributionQuantity(nil))
+	require.Equal(t, uint(0), calcDistributionQuantity(map[uint]uint{}))
+	require.Equal(t, uint(0), calcDistributionQuantity(map[uint]uint{1: 0, 2: 0, 3: 0}))
+	require.Equal(t, uint(1), calcDistributionQuantity(map[uint]uint{1: 1, 2: 0, 3: 0}))
+	require.Equal(t, uint(1), calcDistributionQuantity(map[uint]uint{1: 0, 2: 1, 3: 0}))
+	require.Equal(t, uint(1), calcDistributionQuantity(map[uint]uint{1: 0, 2: 0, 3: 1}))
+	require.Equal(t, uint(2), calcDistributionQuantity(map[uint]uint{1: 1, 2: 1, 3: 0}))
+	require.Equal(t, uint(2), calcDistributionQuantity(map[uint]uint{1: 0, 2: 1, 3: 1}))
+	require.Equal(t, uint(2), calcDistributionQuantity(map[uint]uint{1: 1, 2: 0, 3: 1}))
+	require.Equal(t, uint(3), calcDistributionQuantity(map[uint]uint{1: 1, 2: 1, 3: 1}))
+	require.Equal(t, uint(3), calcDistributionQuantity(map[uint]uint{1: 1, 2: 1, 3: 1}))
+	require.Equal(t, uint(3), calcDistributionQuantity(map[uint]uint{1: 1, 2: 1, 3: 1}))
 }
 
 func TestSafeDivide(t *testing.T) {
