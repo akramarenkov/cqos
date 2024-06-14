23677128f25e7d7797aefec1df9248370e1369a3
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2024-06-15 00:39:09 +0300

    improve readme graphs priority v1

--- go.mod
+++ go.mod
@@ -9,12 +9,16 @@ require (
 	github.com/go-echarts/go-echarts/v2 v2.3.3
 	github.com/guptarohit/asciigraph v0.7.1
 	github.com/stretchr/testify v1.9.0
+	github.com/wcharczuk/go-chart/v2 v2.1.1
 	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df
 )
 
 require (
+	github.com/blend/go-sdk v1.20220411.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/image v0.11.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ priority/priority_readme_graphs_test.go
@@ -0,0 +1,210 @@
+package priority
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/akramarenkov/cqos/internal/consts"
+	"github.com/akramarenkov/cqos/priority/internal/common"
+	"github.com/stretchr/testify/require"
+
+	"github.com/wcharczuk/go-chart/v2"
+	"github.com/wcharczuk/go-chart/v2/drawing"
+)
+
+func TestReadmeGraph(t *testing.T) {
+	t.Run("equaling", func(t *testing.T) { testReadmeGraph(t, true) })
+	t.Run("unmanaged", func(t *testing.T) { testReadmeGraph(t, false) })
+}
+
+func testReadmeGraph(t *testing.T, equaling bool) {
+	t.Parallel()
+
+	if os.Getenv(consts.EnableGraphsEnv) == "" {
+		t.SkipNow()
+	}
+
+	measurerOpts := measurerOpts{
+		HandlersQuantity: 6,
+	}
+
+	msr := newMeasurer(measurerOpts)
+
+	msr.AddWrite(1, 500)
+	msr.AddWrite(2, 500)
+	msr.AddWrite(3, 500)
+
+	msr.SetProcessDelay(1, 100*time.Millisecond)
+	msr.SetProcessDelay(2, 50*time.Millisecond)
+	msr.SetProcessDelay(3, 10*time.Millisecond)
+
+	overTimeResolution := 100 * time.Millisecond
+	overTimeUnit := time.Second
+	overTimeUnitName := "seconds"
+
+	if equaling {
+		opts := Opts[uint]{
+			Divider:          FairDivider,
+			HandlersQuantity: measurerOpts.HandlersQuantity,
+			Inputs:           msr.GetInputs(),
+		}
+
+		discipline, err := New(opts)
+		require.NoError(t, err)
+
+		createReadmeGraph(
+			t,
+			"./doc/different-processing-time-equaling.svg",
+			msr.Play(discipline, false),
+			measurerOpts.HandlersQuantity,
+			msr.GetExpectedItemsQuantity(),
+			overTimeResolution,
+			overTimeUnit,
+			overTimeUnitName,
+		)
+
+		return
+	}
+
+	unmanagedOpts := unmanagedOpts[uint]{
+		Inputs: msr.GetInputs(),
+	}
+
+	unmanaged, err := newUnmanaged(unmanagedOpts)
+	require.NoError(t, err)
+
+	createReadmeGraph(
+		t,
+		"./doc/different-processing-time-unmanagement.svg",
+		msr.Play(unmanaged, false),
+		measurerOpts.HandlersQuantity,
+		msr.GetExpectedItemsQuantity(),
+		overTimeResolution,
+		overTimeUnit,
+		overTimeUnitName,
+	)
+}
+
+func createReadmeGraph(
+	t *testing.T,
+	fileName string,
+	measures []measure,
+	handlersQuantity uint,
+	itemsQuantity uint,
+	overTimeResolution time.Duration,
+	overTimeUnit time.Duration,
+	overTimeUnitName string,
+) {
+	received := filterByKind(measures, measureKindReceived)
+	researched := calcDataQuantity(received, overTimeResolution)
+
+	serieses := make([]chart.Series, 0, len(researched))
+	priorities := make([]uint, 0, len(researched))
+
+	for priority := range researched {
+		priorities = append(priorities, priority)
+	}
+
+	// To keep the legends in the same order
+	common.SortPriorities(priorities)
+
+	for _, priority := range priorities {
+		xaxis := make([]float64, len(researched[priority]))
+		yaxis := make([]float64, len(researched[priority]))
+
+		for id, item := range researched[priority] {
+			xaxis[id] = float64(item.RelativeTime) / float64(overTimeUnit)
+			yaxis[id] = float64(item.Quantity)
+		}
+
+		series := chart.ContinuousSeries{
+			Name:    fmt.Sprintf("Data with priority %d", priority),
+			XValues: xaxis,
+			YValues: yaxis,
+			Style:   chart.Style{StrokeWidth: 4},
+		}
+
+		serieses = append(serieses, series)
+	}
+
+	graph := chart.Chart{
+		Title: "Data retrieval graph" +
+			". " +
+			"Total amount of data: " + strconv.FormatUint(uint64(itemsQuantity), 10) +
+			", " +
+			"handlers quantity: " + strconv.FormatUint(uint64(handlersQuantity), 10),
+		ColorPalette: readmeColorPalette{},
+		Background: chart.Style{
+			Padding: chart.Box{
+				Top:  50,
+				Left: 140,
+			},
+			FillColor: chart.ColorTransparent,
+		},
+		Canvas: chart.Style{
+			FillColor: chart.ColorTransparent,
+		},
+		XAxis: chart.XAxis{
+			Name: "Time, " + overTimeUnitName,
+		},
+		YAxis: chart.YAxis{
+			Name: "Quantity of data received by handlers, pieces",
+		},
+		Series: serieses,
+	}
+
+	graph.Elements = []chart.Renderable{
+		chart.LegendLeft(&graph, chart.Style{FillColor: chart.ColorTransparent}),
+	}
+
+	file, err := os.Create(fileName)
+	require.NoError(t, err)
+
+	err = graph.Render(chart.SVG, file)
+	require.NoError(t, err)
+}
+
+type readmeColorPalette struct{}
+
+func (readmeColorPalette) BackgroundColor() drawing.Color {
+	return chart.DefaultColorPalette.BackgroundColor()
+}
+
+func (readmeColorPalette) BackgroundStrokeColor() drawing.Color {
+	return chart.DefaultColorPalette.BackgroundStrokeColor()
+}
+
+func (readmeColorPalette) CanvasColor() drawing.Color {
+	return chart.DefaultColorPalette.CanvasColor()
+}
+
+func (readmeColorPalette) CanvasStrokeColor() drawing.Color {
+	return chart.DefaultColorPalette.CanvasStrokeColor()
+}
+
+func (readmeColorPalette) AxisStrokeColor() drawing.Color {
+	return chart.DefaultColorPalette.AxisStrokeColor()
+}
+
+func (readmeColorPalette) TextColor() drawing.Color {
+	return chart.DefaultColorPalette.TextColor()
+}
+
+func (readmeColorPalette) GetSeriesColor(index int) drawing.Color {
+	colors := []drawing.Color{
+		{R: 0x54, G: 0x70, B: 0xc6, A: 255},
+		{R: 0x91, G: 0xcc, B: 0x75, A: 255},
+		{R: 0xfa, G: 0xc8, B: 0x58, A: 255},
+		{R: 0xee, G: 0x66, B: 0x66, A: 255},
+		{R: 0x73, G: 0xc0, B: 0xde, A: 255},
+		{R: 0x3b, G: 0xa2, B: 0x72, A: 255},
+		{R: 0xfc, G: 0x84, B: 0x52, A: 255},
+		{R: 0x9a, G: 0x60, B: 0xb4, A: 255},
+		{R: 0xea, G: 0x7c, B: 0xcc, A: 255},
+	}
+
+	return colors[index%len(colors)]
+}
