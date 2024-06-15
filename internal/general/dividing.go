82f4eb452067d6beaae70553b0ba0a527780db22
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2024-06-16 02:26:03 +0300

    add dividing desc

--- internal/general/dividing.go
+++ internal/general/dividing.go
@@ -4,6 +4,8 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
+// Divides base value on to divider. Returns base value if divider is zero.
+// Returns min value if result of dividing is less than min.
 func DivideWithMin[Type constraints.Integer](base Type, divider Type, min Type) Type {
 	if divider == 0 {
 		return base
