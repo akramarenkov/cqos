912598f1cd97bedc879e4f494a8e4c1af6f4bba8
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2024-06-16 01:52:07 +0300

    simply dividing test

--- v2/internal/general/dividing_test.go
+++ v2/internal/general/dividing_test.go
@@ -7,27 +7,13 @@ import (
 )
 
 func TestDivideWithMin(t *testing.T) {
-	reduced := DivideWithMin(10, 0, 3)
-	require.Equal(t, 10, reduced)
-
-	reduced = DivideWithMin(10, 1, 3)
-	require.Equal(t, 10, reduced)
-
-	reduced = DivideWithMin(10, 2, 3)
-	require.Equal(t, 5, reduced)
-
-	reduced = DivideWithMin(10, 3, 3)
-	require.Equal(t, 3, reduced)
-
-	reduced = DivideWithMin(10, 4, 3)
-	require.Equal(t, 3, reduced)
-
-	reduced = DivideWithMin(10, 5, 3)
-	require.Equal(t, 3, reduced)
-
-	reduced = DivideWithMin(10, 10, 3)
-	require.Equal(t, 3, reduced)
-
-	reduced = DivideWithMin(10, 11, 3)
-	require.Equal(t, 3, reduced)
+	require.Equal(t, 10, DivideWithMin(10, 0, 3))
+	require.Equal(t, 10, DivideWithMin(10, 1, 3))
+	require.Equal(t, 5, DivideWithMin(10, 2, 3))
+	require.Equal(t, 3, DivideWithMin(10, 3, 3))
+	require.Equal(t, 3, DivideWithMin(10, 4, 3))
+	require.Equal(t, 3, DivideWithMin(10, 5, 3))
+	require.Equal(t, 3, DivideWithMin(10, 10, 3))
+	require.Equal(t, 3, DivideWithMin(10, 11, 3))
+	require.Equal(t, 3, DivideWithMin(10, 12, 3))
 }
